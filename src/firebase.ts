6bc4d3f14644038bc537d91b58fd72ddebcbf4fe
김여명 <yeokim5@soft11.co.kr>
2025-05-14 11:17:44 +0900

    ignore

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,13 +8,14 @@ const firebaseConfig = {
   authDomain: "vivid-6d6cd.firebaseapp.com",
   projectId: "vivid-6d6cd",
   storageBucket: "vivid-6d6cd.firebasestorage.app",
-  messagingSenderId: "463862134798",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: "1:463862134798:web:b005e3598d61bd401b245f",
   measurementId: "G-GDMBPWW5BM",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 // Get auth instance
 export const auth = getAuth(app);
 export default app;
